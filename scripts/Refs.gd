73b596971736947b2e0f5d35abc90098ed74edc0
anthony <anthony.gregori78200@gmail.com>
2024-11-03 14:06:10 -0500

    adding a test for refs and export group

---
+++ scripts/Refs.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@export_group("ColorPalette")
+@export var color_blue : Color = Color("#00303b")
+@export var color_red : Color = Color("#ff7777")
+@export var color_whitish : Color = Color("#f1f2da")
+@export var color_yellow : Color = Color("#ffce96")
