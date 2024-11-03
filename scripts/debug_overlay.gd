f351023e960e747d1879cddc907caaf75c548dd9
anthony <anthony.gregori78200@gmail.com>
2024-11-03 15:18:03 -0500

    Enhance debug overlay functionality: update add_stat method to accept additional parameters and modify value retrieval logic

--- scripts/debug_overlay.gd
+++ scripts/debug_overlay.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 # Debug overlay by Gonkee - full tutorial https://youtu.be/8Us2cteHbbo
 var stats = []
 
-func add_stat(stat_name, object, stat_ref, is_method):
-	stats.append([stat_name, object, stat_ref, is_method])
+func add_stat(stat_name, object, stat_ref, is_method, params=[]):
+	stats.append([stat_name, object, stat_ref, is_method, params])
 
 func _process(_delta):
 	var label_text = ""
@@ -22,7 +22,10 @@ func _process(_delta):
 		
 		if s[1] and weakref(s[1]).get_ref():
 			if s[3]:
-				value = s[1].call(s[2])
+				if len(s[4]) == 0:
+					value = s[1].call(s[2])
+				else:
+					value = s[1].callv(s[2], s[4])
 			else:
 				value = s[1].get(s[2])
 		label_text += str(s[0], ": ", value)
