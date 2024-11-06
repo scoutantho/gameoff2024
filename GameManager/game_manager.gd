28b0db241c7ba1f5ba20277df0bbe3c4dd252807
anthony <anthony.gregori78200@gmail.com>
2024-11-05 19:59:51 -0500

    Refactor GameManager and DrawableLine: update variable names for platform creation toggle and fix signal connections

--- GameManager/game_manager.gd
+++ GameManager/game_manager.gd
@@ -1,20 +1,26 @@
 extends Node2D
 
 #todo replace with an enum and a colorPicker or something like that, saw it on a YT video
-signal isCreatePlateformeEnabling
 static var colors = ["","",""] 
 var playerHasDied = false
 var isDisplayOverlayEnable: bool = true
-var isCreatePlateformeEnabled : bool : 
+
+signal isCreatePlateformeEnabling
+var isCreatePlateformeEnable : bool : 
 	set(value): 
 		if value:
 			isCreatePlateformeEnabling.emit() #emit signal, pulled back in the DrawableLine script in order to activate or deactivate it (in the futur probably we will think about adding in the tree or removing it from the tree
-		isCreatePlateformeEnabled = value
+		isCreatePlateformeEnable = value
+	set(value): 
+		if value:
+			isCreatePlateformeEnabling.emit() #emit signal, pulled back in the DrawableLine script in order to activate or deactivate it (in the futur probably we will think about adding in the tree or removing it from the tree
+		isCreateSquareEnable = value
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	isCreatePlateformeEnabled = false
+	isCreatePlateformeEnable = false
+	isCreateSquareEnable = false
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/DrawableLine.gd
+++ scripts/DrawableLine.gd
@@ -14,7 +14,7 @@ func displayOverlayDebugging():
 		# DebugOverlay.add_stat("position1", $Line2D, "get_point_position", true, [1])
 	
 func _ready():
-	if GameManager.isCreatePlateformeEnabled:
+	if GameManager.isCreatePlateformeEnable:
 		InitLine2D()
 		pass
 	GameManager.connect("isCreatePlateformeEnabling", InitLine2D)	
@@ -26,7 +26,7 @@ func InitLine2D() -> void :
 	pass
 	
 func _input(event: InputEvent) -> void:
-	if GameManager.isCreatePlateformeEnabled:
+	if GameManager.isCreatePlateformeEnable:
 		if event is InputEventMouseButton && event.pressed && event.button_index == MOUSE_BUTTON_LEFT && isDrawableSpace && not isDrawingLocked:
 			print("i want to draw something")
 			var mousePosition := get_global_mouse_position()		
@@ -64,5 +64,5 @@ func draw_my_cubes():
 	
 
 func _process(_delta):
-	if GameManager.isCreatePlateformeEnabled:
+	if GameManager.isCreatePlateformeEnable:
 		lineFunction()
