e3dc633abf61ddee0e5978e824353bd4c1e5ed69
anthony <anthony.gregori78200@gmail.com>
2024-11-09 09:51:22 -0500

    change bush offset on bottom in order to scale it properly Add bush growth script and update bush scene: implement water interaction and scaling logic

---
+++ scripts/bush.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var numberOfWater = 0
+@export var isAlreadyGrowing = false
+
+func _on_body_entered(body:Node2D) -> void:
+	if(body.is_in_group("water")):
+		print("adding one water")
+		numberOfWater += 1
+	pass # Replace with function body.
+
+func _process(_delta: float) -> void:
+	if (numberOfWater >= 10 && isAlreadyGrowing == false):
+		print("I SHOULD GROW")
+		var actualScale = self.scale
+		self.scale = Vector2(actualScale.x, actualScale.y * 2)
+		isAlreadyGrowing = true
+
+	pass # Replace with function body.
