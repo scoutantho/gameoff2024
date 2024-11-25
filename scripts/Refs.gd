5b0856fc27c2750a7f472a1228d6c498b224c573
anthony <anthony.gregori78200@gmail.com>
2024-11-25 13:47:29 -0500

    in order to handle each sprite with a different color it is necessary to creata a child for each and not edit the existign sprite, editing existing sprite fait que ça edit tous les sprites de cet objet

--- scripts/Refs.gd
+++ scripts/Refs.gd
@@ -4,3 +4,8 @@ extends Node
 @export var color_red : Color = Color("#ff7777")
 @export var color_whitish : Color = Color("#f1f2da")
 @export var color_yellow : Color = Color("#ffce96")
+
+@export_group("Gradients")
+@export var gradient_blue : PackedColorArray = PackedColorArray([color_whitish, color_blue])
+@export var gradient_red : PackedColorArray = PackedColorArray([color_whitish, color_red])
+@export var gradient_yellow : PackedColorArray = PackedColorArray([color_whitish, color_yellow])
--- scripts/non_drawable_bush.gd
+++ scripts/non_drawable_bush.gd
@@ -28,3 +28,32 @@ func _on_body_entered(body:Node2D) -> void:
 			if body.is_in_group("water"):
 				body.queue_free()
 	pass # Replace with function body.
+
+func _ready() -> void:
+	print("powerUpWhoCantDraw:", powerUpWhoCantDraw)
+	var packedColorArray: PackedColorArray
+	match powerUpWhoCantDraw:
+		Globals.powerUp.bush:
+			packedColorArray = Refs.gradient_yellow
+		Globals.powerUp.lava:
+			packedColorArray = Refs.gradient_red
+		Globals.powerUp.water:
+			packedColorArray = Refs.gradient_blue
+	addSpriteWithRadialGradient(packedColorArray)
+	print("lol")
+			
+	# texture.gradient.set_color(1, colorTo)
+	# texture.gradient = texture.gradient.interpolate_colors(colorFrom, colorTo)
+
+func addSpriteWithRadialGradient(packedColor: PackedColorArray) -> void:
+	var texture = GradientTexture2D.new()
+	texture.gradient = Gradient.new()
+	texture.fill = texture.FILL_RADIAL
+	texture.fill_from = Vector2(0.5, 0.5)
+	texture.fill_to = Vector2(0.83, 0)
+	var sprite = Sprite2D.new()
+	sprite.texture = texture
+	sprite.texture.gradient.set_color(0, packedColor[0])
+	sprite.texture.gradient.set_color(1, packedColor[1])
+	sprite.texture = sprite.texture
+	self.add_child(sprite)
